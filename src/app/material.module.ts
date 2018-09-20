3b70088c8250d991fb50699bf7c018cecb0023ba
Fernando <fernando.galarcio@gmail.com>
2018-09-20 01:37:33 -0500

    Finish basic forms angular material

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,32 @@
-import { NgModule } from "@angular/core";
-import { MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule } from '@angular/material';
+import { NgModule } from '@angular/core';
+import { MatButtonModule,
+         MatIconModule,
+         MatFormFieldModule,
+         MatInputModule,
+         MatDatepickerModule,
+         MatNativeDateModule,
+         MatCheckboxModule
+        } from '@angular/material';
 
 @NgModule({
-    imports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule],
-    exports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule]
+    imports: [
+        MatButtonModule,
+        MatIconModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatCheckboxModule
+    ],
+    exports: [
+        MatButtonModule,
+        MatIconModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatCheckboxModule
+    ]
 })
 
 export class MaterialModule {
